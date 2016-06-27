c1e77c2be810821904f0d188bb714ff868972fa1
NatoliChris <natolichris@gmail.com>
2016-06-27 11:02:09 +1000

    Playing with values, learning constants, understanding defining variables

---
+++ learning_values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+
+func prt(msg string){
+	fmt.Println(msg)
+}
+
+func main(){
+	prt("go" + "lang")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+}
